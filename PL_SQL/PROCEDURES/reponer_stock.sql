6fa8535b59f5044e3dac817a5cf2a70d6ff14dbc
Alvaro Vidal <alvaro.vidal1403@gmail.com>
2025-12-13 20:16:46 -0500

    procedure reponer_stock

---
+++ PL_SQL/PROCEDURES/reponer_stock.sql
@@ -0,0 +1,13 @@
+
+
+
+CREATE OR REPLACE PROCEDURE reponer_stock(
+    entrada_producto_id IN NUMBER,
+    entrada_cantidad IN NUMBER
+) IS
+BEGIN
+    UPDATE inventarios
+    SET stock = stock + entrada_cantidad
+    WHERE producto_id = entrada_producto_id;
+END;
+/
