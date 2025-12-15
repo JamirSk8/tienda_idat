7243f6f6041e33c444c296e1e2be214aacef5ba2
JamirSk8 <f1276255@pucp.edu.pe>
2025-12-14 22:17:03 -0500

    volumen tabla productos
    volumen

--- DML/INSERT/productos.sql
+++ DML/INSERT/productos.sql
@@ -5,3 +5,138 @@ VALUES ('RAM HyperX 32GB', 1, 1, 500);
 
 INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
 VALUES ('Mouse Logitech G203', 2, 2, 120);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Teclado Mecánico Redragon', 2, 2, 180);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Monitor Samsung 24"', 3, 3, 700);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Disco SSD Kingston 1TB', 1, 1, 350);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Tarjeta Gráfica NVIDIA RTX 3060', 1, 4, 2200);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Auriculares Corsair HS50', 2, 2, 250);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Procesador AMD Ryzen 5', 1, 1, 950);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Placa Madre ASUS B450', 1, 1, 600);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Webcam Logitech C920', 2, 2, 320);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Impresora HP DeskJet', 5, 6, 400);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Router TP-Link Archer', 8, 8, 180);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Disco Duro Externo Seagate 2TB', 1, 1, 420);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Fuente de Poder EVGA 600W', 1, 1, 280);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Mousepad Razer Goliathus', 2, 2, 60);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Lector de Huellas Digitales', 9, 9, 150);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Switch de Red Cisco', 8, 8, 800);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Laptop Dell XPS 13', 3, 10, 4800);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Smartphone Apple iPhone 14', 6, 11, 5200);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Tablet Apple iPad Air', 6, 11, 3200);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Monitor Samsung Odyssey', 3, 3, 2100);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Mouse Logitech MX Master 3', 2, 2, 350);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Teclado Razer BlackWidow', 2, 12, 600);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Auriculares Sony WH-1000XM4', 2, 13, 1200);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Parlante Bose SoundLink', 10, 14, 900);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Disco SSD Crucial MX500 1TB', 1, 1, 420);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Impresora Canon PIXMA G3110', 5, 7, 650);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Router ASUS RT-AX88U', 8, 15, 1200);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Cámara GoPro HERO11', 7, 16, 1800);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Smartwatch Samsung Galaxy Watch 5', 11, 3, 1100);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Disco Duro Externo WD My Passport 2TB', 1, 17, 480);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Switch Cisco Catalyst 2960', 8, 8, 2200);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Laptop HP Spectre x360', 3, 18, 4700);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Smartphone Samsung Galaxy S23', 6, 3, 3900);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Tablet Lenovo Tab P11', 6, 19, 1800);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Monitor LG UltraWide 34WL500', 3, 20, 1600);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Mouse Microsoft Surface Arc', 2, 21, 320);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Teclado Corsair K95 RGB', 2, 22, 850);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Auriculares JBL Tune 510BT', 2, 23, 300);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Parlante Sony SRS-XB43', 10, 13, 700);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Disco SSD Kingston A400 480GB', 1, 1, 250);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Impresora Epson L3250', 5, 24, 800);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Router TP-Link Deco X20', 8, 8, 950);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Cámara Nikon D3500', 7, 25, 2100);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Smartwatch Amazfit GTR 4', 11, 26, 900);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Disco Duro Externo Seagate Expansion 1TB', 1, 1, 400);
+
+INSERT INTO productos (producto, categoria_id, proveedor_id, precio_actual)
+VALUES ('Switch Netgear GS308', 8, 27, 600);
